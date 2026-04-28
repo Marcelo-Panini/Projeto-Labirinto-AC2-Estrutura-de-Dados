9a371fa9e42309dfb75f68e3af4e4296db070161
Marcelo Panini da Silva <paninimarcelosilvap@gmail.com>
2026-04-28 09:15:55 -0300

    v0.3.1 Adicionada Função para Imprimir a mochila

--- main.c
+++ main.c
@@ -19,21 +19,21 @@ typedef struct{
 
 //função que le o arquivo .txt e carrega o mapa.
 void carregaLabirinto(Labirinto *l, const char *nomeArquivo){
-    
+
     FILE *arquivo = fopen(nomeArquivo, "r"); //abre o arquivo para a leitura
 
     if(arquivo == NULL){ //tratamento de erro no caso do arquivo não existir ou n poder ser aberto
         printf("Erro ao abrir arquivo %s\n", nomeArquivo); //mensagem do erro
         exit(1);
     }
-    
+
         //le a primeira linha do arquivo .txt para configurar as dimensões do mapa
-        fscanf(arquivo, "%dx%d\n", &l->linhas, &l->colunas); 
+        fscanf(arquivo, "%dx%d\n", &l->linhas, &l->colunas);
 
         //loop que percorre cada linha do labirinto
         for(int i = 0; i < l->linhas; i++){
             fgets(l->mapa[i], MAX + 2, arquivo); //le toda a linha preservando espaços vazios que são os corredores
-        
+
         //loop que analisa cada coluna da linha
         for(int j = 0; j < l->colunas; j++){
 
@@ -46,7 +46,7 @@ void carregaLabirinto(Labirinto *l, const char *nomeArquivo){
             //remove quebra de linhas para não danificar a exibição visual
             if(l->mapa[i][j] == '\n' || l->mapa[i][j] == '\r'){
                 l->mapa[i][j] = '\0';
-            } 
+            }
         }
     }
 
@@ -54,12 +54,45 @@ void carregaLabirinto(Labirinto *l, const char *nomeArquivo){
 }
 
 //função para guardar item na mochila
-void guardaMochila(No **topo; int valorNovo){
+void guardaMochila(No **topo, int valorNovo){
 
-    //aloca memoria para o novo no
+    //alocação dinamica na memoria para novo tesouro
     No  *novo = (No*) malloc(sizeof(No));
     novo->valor = valorNovo;
     novo->prox = NULL;
+
+    //se a mochila estiver vazia ou o tesouro tiver o menor valor, entra na primera posição
+    if(*topo == NULL || valorNovo < (*topo)->valor){
+        novo->prox = *topo;
+        *topo = novo;
+    }
+
+    //caso n seja o menor valor, procura a posição correta na ordem crescente dos valores
+    else{
+        No *atual = *topo;
+
+        //loop para percorrer a lista enquanto o proximo valor ainda for menor que o novo
+        while(atual->prox != NULL && atual->prox->valor < valorNovo){
+            atual = atual->prox;
+        }
+
+        //faz o encadeamento, fazendo o novo valor apontar para o proximo e o atual apontar para o novo
+        novo->prox = atual->prox;
+        atual->prox = novo;
+    }
+}
+
+//função que mostra a mochila
+void imprimeMochila(No *topo){
+    printf("Moxhila: ");
+    No *atual = topo;
+
+    //percorre toda a lista até o fim
+    while(atual != NULL){
+        printf("%i ", atual->valor);
+        atual = atual->prox;
+    }
+    printf("\n");
 }
 
 int main() {
@@ -75,5 +108,6 @@ int main() {
         printf("%s\n", labi.mapa[i]);
     }
 
-    return 0; 
+    return 0;
 }
+
